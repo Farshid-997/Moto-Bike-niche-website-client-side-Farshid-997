a488b96fa37018db80d09ac441fb0433c3bf2ab5
Farshid-997 <arnob.farshid@gmail.com>
2021-11-10 12:44:31 +0600

    commited

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ 
+} from "react-router-dom";
+import Home from './Pages/Home/Home';
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+      <Switch>
+         
+      <Route exact path="/">
+      <Home></Home>
+          </Route>
+          
+          <Route path="/users">
+            <Home />
+          </Route>
+
+          {/* <Route path="/login">
+         <Login></Login>
+          </Route>
+
+          <Route path="/register">
+       <Register></Register>
+          </Route>
+
+          <Route path="/dashboard">
+     <Dashboard></Dashboard>
+          </Route> */}
+
+         
+
+
+        </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import bike from '../../../Images/wp3647905-black-bike-wallpapers.jpg'
+import rider from '../../../Images/biking-motorcycle-forest.jpg'
+import { Button, Container, Typography } from '@mui/material';
+
+
+const backgroundbg={
+    background:`url(${bike})`,
+   
+}
+const verticalAlign={
+    display:'flex',
+  alignItems:'center',
+    height:450,
+  
+   
+}
+const Banner = () => {
+    return (
+        <div style={backgroundbg}>
+<Container  sx={{ flexGrow: 1 }}>
+        <Grid    container spacing={2}>
+          <Grid item xs={12} sm={4} md={5} style={{...verticalAlign,textAlign:'left'}}>
+  
+  
+         <Box>
+  
+         <Typography varirant="h3" style={{color:'white'}}>
+  Bike Sells <br/>
+  Starts Here
+          </Typography>
+  
+          <Typography varirant="h6" style={{fontSize:14,fontWeight:300,color:'white'}}>
+ We Have Some amazing bike Collections
+          </Typography>
+  
+          <Button variant="contained" sx={{mt:2}} style={{backgroundColor:"#5CE7ED"}}>Get the Bike</Button>
+         </Box>
+  
+          </Grid>
+  
+          <Grid item xs={12} sm={8} md={7} style={verticalAlign}>
+       <img style={{width:'350px'}} src={rider} alt="" /> 
+          </Grid>
+       
+         
+        </Grid>
+      </Container>  
+        </div>
+        
+    );
+};
+
+export default Banner;
